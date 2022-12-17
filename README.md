bc76c775bac01f5eea8f75142733826925cbcdb8
shahinovic <https://github.com/shahinovic>
2022-12-17 21:36:52 +0200

    Some required changes

--- README.md
+++ README.md
@@ -2,12 +2,38 @@
 
 ## Table of Contents
 
-* [Instructions](#instructions)
+- [Description](#Description)
+- [Development](#Development)
+- [Instructions](#instructions)
 
-## Instructions
+## Description
 
-The starter project has some HTML and CSS styling to display a static version of the Landing Page project. You'll need to convert this project from a static project to an interactive one. This will require modifying the HTML and CSS files, but primarily the JavaScript file.
+I started the project with some HTML and CSS styling to display a static version of the Landing Page project.
 
-To get started, open `js/app.js` and start building out the app's functionality
+I converted this project from a static project to an interactive one
 
-For specific, detailed instructions, look at the project instructions in the Udacity Classroom.
+I made some changes in index.js and css/styles.css files, and make the functionality of the project in js/app.js file.
+
+## Development
+
+- make the structure of Project.
+
+- Styling has been added for active states.
+
+- Navigation is built dynamically as an unordered list.
+
+- It should be clear which section is being viewed while scrolling through the page.
+
+- make a scroll spy
+
+- All features are usable across modern desktop, tablet, and phone browsers.
+
+  - make a responsive navbar
+  - add media queries to .landing\_\_container
+  - used font-size: clamp(MIN, VAL, MAX); Property to make most of fonts responsive
+
+- There are at least 4 sections that have been added to the page.
+
+- The ReadMe file should have non-default text in it.
+
+- Code is formatted with consistent, logical, and easy-to-read formatting.
--- js/app.js
+++ js/app.js
@@ -1,8 +1,12 @@
-const header = document.querySelector('.page__header'),
-  list = document.querySelector('#navbar__list'),
-  sections = Array.from(document.querySelectorAll('section')),
-  toggler = document.createElement('button'),
-  svg = `<svg width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
+// # start global variables
+
+const header = document.querySelector('.page__header');
+const list = document.querySelector('#navbar__list');
+const sections = Array.from(document.querySelectorAll('section'));
+const toggler = document.createElement('button');
+const bar = document.createElement('span');
+const spans = `<span></span><span></span><span></span><span></span>`;
+const svg = `<svg width="24" height="25" viewBox="0 0 24 25" fill="none" xmlns="http://www.w3.org/2000/svg">
   <g clip-path="url(#clip0_2_46)">
   <path d="M21.2211 0.893066L24 3.92465L12.1263 15.7983L0 3.92465L3.03158 0.893066L12.1263 9.9878L21.2211 0.893066Z" fill="white"/>
   </g>
@@ -12,34 +16,20 @@ const header = document.querySelector('.page__header'),
   </clipPath>
   </defs>
   </svg>
-  `,
-  spans = `<span></span><span></span><span></span><span></span>`,
-  collapsedH2 = document.querySelectorAll('.landing__container h2'),
-  btnHover = document.querySelectorAll('.btn-hover');
+  `;
+const collapsedH2 = document.querySelectorAll('.landing__container h2');
+const btnHover = document.querySelectorAll('.btn-hover');
+let timer = null;
 
-window.addEventListener('load', () => {
-  btnHover.forEach((ele) => {
-    ele.innerHTML += spans;
-  });
-  collapsedH2.forEach((ele) => {
-    ele.innerHTML += svg;
-  });
-});
+// # end global variable
 
-let scroller = (e) => {
-  e.preventDefault();
-  let sec = document.querySelector(`${e.target.getAttribute('href')}`).offsetTop;
-  window.scrollTo({
-    top: sec,
-    behavior: 'smooth'
-  });
-};
+// # start helper functions
+
 // add link to the list
 
 let AddLinks = (ele) => {
-  let li = document.createElement('li'),
-    a = document.createElement('a');
-
+  let li = document.createElement('li');
+  let a = document.createElement('a');
   a.textContent = ele.dataset.nav;
   a.setAttribute('href', `#${ele.id}`);
   a.className = `menu__link ${ele.id}`;
@@ -53,23 +43,22 @@ let AddLinks = (ele) => {
   list.appendChild(li);
 };
 
-// looping in the sections and make a link to each section
-sections.map(AddLinks);
-// scroll smoothly function
+// this function made for smooth scrolling
 
-// toggle menu button
-toggler.setAttribute('id', 'toggler');
-for (let i = 0; i < 3; i++) {
-  let bar = document.createElement('span');
-  bar.classList.add('bar');
-  toggler.appendChild(bar);
-}
-document.querySelector('.navbar__menu').appendChild(toggler);
+let scroller = (e) => {
+  e.preventDefault();
+  let sec = document.querySelector(`${e.target.getAttribute('href')}`).offsetTop;
+  window.scrollTo({
+    top: sec,
+    behavior: 'smooth'
+  });
+};
 
-toggler.addEventListener('click', () => {
+// open and close the navbar menu
+const toggleFunction = () => {
   list.classList.toggle('open');
   toggler.classList.toggle('close');
-});
+};
 
 // add active class to the section in the viewport
 
@@ -84,31 +73,73 @@ const shower = new IntersectionObserver((entries) => {
     }
   });
 });
+
+// this function for hide the header
+const transNav = () => {
+  document.querySelector('header').style = 'transform: translateY(-100px);';
+  list.classList.remove('open');
+  toggler.classList.remove('close');
+};
+
+// this function to make a delay before hide the navbar
+
+const hiddenNavbar = () => {
+  timer !== null ? clearTimeout(timer) : null;
+  timer = setTimeout(transNav, 3000);
+};
+
+// # end helper functions
+
+/*
+when the function load this function will
+ - add spans -important part for animation- to all the elements witch have btn-hover class
+ - add svg to the h2 on every element has class landing__container
+  - for know this section can be collapsed
+
+*/
+window.addEventListener('load', () => {
+  btnHover.forEach((ele) => (ele.innerHTML += spans));
+  collapsedH2.forEach((ele) => (ele.innerHTML += svg));
+});
+
+// looping in the sections and make a link to each section
+sections.map(AddLinks);
+
+// add id 'toggler' to the toggle btn
+
+toggler.setAttribute('id', 'toggler');
+
+// add bars to toggler btn
+
+for (let i = 0; i < 3; i++) {
+  bar.classList.add('bar');
+  toggler.appendChild(bar);
+}
+
+// add the toggler btn to the navbar
+
+document.querySelector('.navbar__menu').appendChild(toggler);
+
+// open and close navbar menu when user click the toggler btn
+
+toggler.addEventListener('click', toggleFunction);
+
+// add class active to the section on the view port
+
 document.querySelectorAll('section').forEach((ele) => shower.observe(ele));
 
 // hide the navbar when the scroll stop
 
-let timer = null;
-window.addEventListener(
-  'scroll',
-  () => {
-    if (timer !== null) {
-      clearTimeout(timer);
-    }
-    timer = setTimeout(() => {
-      document.querySelector('header').style = 'transform: translateY(-100px);';
-      list.classList.remove('open');
-      toggler.classList.remove('close');
-    }, 4000);
-  },
-  false
-);
+window.addEventListener('scroll', hiddenNavbar, false);
+
+// show navbar when user scroll scroll
+
 window.onscroll = () => {
   document.querySelector('header').style = 'transform: translateY(0);';
 };
 
-Array.from(collapsedH2).map((ele) => {
-  ele.addEventListener('click', () => {
-    ele.parentElement.classList.toggle('collapse');
-  });
-});
+// open and close sections when user click on the h2 element
+
+Array.from(collapsedH2).map((ele) =>
+  ele.addEventListener('click', () => ele.parentElement.classList.toggle('collapse'))
+);
